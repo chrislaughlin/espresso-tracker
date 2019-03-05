3d4801bc464db7e3fd20074cc305cc572d57f258
Chris Laughlin <bluntm@gmail.com>
2019-03-05 23:56:38 +0000

    adding logo

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import Title from "../../components/title/Title";
 import MainMenu from "./MainMenu";
+import Logo from './logo.png';
+
+const StyledHome = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const StyledLogo = styled.img.attrs({
+    src: Logo,
+    alt: 'Espresso Tracker Logo'
+})`
+    height: 180px;
+    width: 185px;
+    margin: 0 auto;
+`;
 
 const Home = () => {
     return (
-        <div>
+        <StyledHome>
+            <StyledLogo/>
             <Title/>
             <MainMenu/>
-        </div>
+        </StyledHome>
     );
 };
 
